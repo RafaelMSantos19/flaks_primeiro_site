dbda015cf049882c7e8c16da4335784a48f8732c
Rafael <rafarag40@gmail.com>
2022-07-16 10:12:31 -0300

    modificaçoes001

--- flask_tutorial.py
+++ flask_tutorial.py
@@ -15,6 +15,10 @@ def hello_word():
 def contato():
     return render_template("contatos.html")
 
+@app.route("/Rafael")
+def rafael():
+    return render_template("rafael.html")   
+
 @app.route("/usuarios/<name>")
 def usuario(name):
     return render_template("usuarios.html",name=name)
@@ -24,3 +28,4 @@ def usuario(name):
 #colocar o site no ar
 if __name__ == "__main__":
     app.run(debug=True)
+
