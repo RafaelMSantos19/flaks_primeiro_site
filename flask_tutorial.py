4a8c13a959b6b584c9b50cb99452b2046821501c
Rafael <rafarag40@gmail.com>
2022-07-16 09:49:39 -0300

    primeiro up

---
+++ flask_tutorial.py
@@ -0,0 +1,26 @@
+from flask import Flask , render_template
+
+app = Flask(__name__)
+
+
+#cria primeiro site
+
+#router
+@app.route("/")
+#função
+def hello_word():
+     return render_template("index.html")
+
+@app.route("/contato")
+def contato():
+    return render_template("contatos.html")
+
+@app.route("/usuarios/<name>")
+def usuario(name):
+    return render_template("usuarios.html",name=name)
+
+
+
+#colocar o site no ar
+if __name__ == "__main__":
+    app.run(debug=True)
